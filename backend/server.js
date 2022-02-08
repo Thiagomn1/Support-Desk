8cfac5caaa7017e0a2b94016ead43454e29b5d48
Thiago Nascimbeni <tnascimbeni@gmail.com>
2022-02-08 18:12:49 -0300

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const colors = require("colors")
+const dotenv = require("dotenv").config()
+const { errorHandler } = require("./middleware/ErrorMiddleware")
+const connectDB = require("./config/db")
+const PORT = process.env.PORT || 5000
+
+// Connect to database
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the Support Desk API" })
+})
+
+// Routes
+app.use("/api/users", require("./routes/UserRoutes"))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
