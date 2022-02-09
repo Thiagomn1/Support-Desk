1cd7251e9350f87a92211ba30f8cd0066e812cde
Thiago Nascimbeni <tnascimbeni@gmail.com>
2022-02-09 18:46:01 -0300

    Authentication and private routes

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express")
 const router = express.Router()
-const { registerUser, loginUser } = require("../controllers/UserController")
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require("../controllers/UserController")
+
+const { protect } = require("../middleware/AuthMiddleware")
 
 router.post("/", registerUser)
 
 router.post("/login", loginUser)
+
+router.get("/me", protect, getMe)
 
 module.exports = router
