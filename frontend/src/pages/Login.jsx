08844f323c2b6b8626223582ed1d12471540fe03
Thiago Nascimbeni <tnascimbeni@gmail.com>
2022-02-11 14:14:35 -0300

    Login and logout functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,97 @@
+import { useState, useEffect } from "react"
+import { toast } from "react-toastify"
+import { FaSignInAlt } from "react-icons/fa"
+import { useSelector, useDispatch } from "react-redux"
+import { login, reset } from "../features/auth/AuthSlice"
+import { useNavigate } from "react-router-dom"
+import Spinner from "../components/Spinner"
+
 function Login() {
-  return <div>Login</div>
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  })
+
+  const { email, password } = formData
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const { user, isLoading, isSuccess, isError, message } = useSelector(state => state.auth)
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    if (isSuccess || user) {
+      navigate("/")
+    }
+
+    dispatch(reset())
+  }, [isError, isSuccess, user, message, navigate, dispatch])
+
+  const onChange = event => {
+    setFormData(prevState => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }))
+  }
+
+  const onSubmit = event => {
+    event.preventDefault()
+    const userData = {
+      email,
+      password,
+    }
+
+    dispatch(login(userData))
+  }
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please login to get support</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Enter your Email"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              onChange={onChange}
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  )
 }
 
 export default Login
